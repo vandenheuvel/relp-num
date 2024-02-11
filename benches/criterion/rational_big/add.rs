73ca8f844ba7812e25e1562700bb9fc648b67ea4
Bram van den Heuvel <bram@vandenheuvel.online>
2024-02-11 23:23:07 +0100

    Update dependencies

--- benches/criterion/rational_big/add.rs
+++ benches/criterion/rational_big/add.rs
@@ -42,10 +42,19 @@ fn small_with_small_other_denominator(c: &mut Criterion) {
     }));
 }
 
+fn large_with_large_other_denominator(c: &mut Criterion) {
+    c.bench_function("RationalBig: large + large, other denominator", |b| b.iter(|| {
+        let x = black_box(RationalBig::from_str("6168468435984654684986468468465846854869699989985/5648654685465465464684846546846856725").unwrap());
+        let y = black_box(RationalBig::from_str("616846843598465468438574687768468765846854869699989985/564846898987498778846546846856725").unwrap());
+        x + y
+    }));
+}
+
 criterion_group!(group,
     zero_with_small,
     small_with_zero,
     small_with_small_same_denominator,
     small_with_small_other_denominator,
     int_with_int,
+    large_with_large_other_denominator,
 );
--- benches/iai/mod.rs
+++
@@ -1,9 +0,0 @@
-use rational_big::add::{large_with_large_other_denominator, small_with_small_other_denominator, small_with_small_same_denominator};
-
-pub mod rational_big;
-
-iai::main!(
-    large_with_large_other_denominator,
-    small_with_small_same_denominator,
-    small_with_small_other_denominator,
-);
